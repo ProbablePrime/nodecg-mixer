0743f1fe3a8ff03e79a56902b95ffce61f58ccea
Richard Fox <prime@ahref.co.uk>
2016-02-29 03:28:54 +0000

    Store all events in a databse to prevent duplicates

--- extension.js
+++ extension.js
@@ -2,19 +2,14 @@
 
 var nodecg;
 var Channel = require('./lib/Channel.js');
-var channelCache = {};
-
-var Beam = require('beam-client-node');
-
 var LiveLoading = require('./lib/Live.js');
+var channelCache = {};
 
 var live;
 
-var client = new Beam();
-
 module.exports = function (extensionApi) {
 	nodecg = extensionApi;
-	if(!nodecg.bundleConfig || !Object.keys(nodecg.bundleConfig).length) {
+	if (!nodecg.bundleConfig || !Object.keys(nodecg.bundleConfig).length) {
 		throw new Error('No config found in cfg/nodecg-beam.json, aborting!');
 	}
 
@@ -24,53 +19,83 @@ module.exports = function (extensionApi) {
 
 	function log(msg) {
 		nodecg.log.info(msg);
-		nodecg.sendMessage('log',msg);
+		nodecg.sendMessage('log', msg);
 	}
 
-	var onFollow = function(channel,username) {
+	var onFollow = function (channelName, username) {
 		var content = {
 			name: username,
-			channel: channel,
+			channel: channelName,
 			ts: Date.now()
 		};
 		log('follow ' + username);
 		nodecg.sendMessage('follow', content);
 	};
 
-	var onSub = function(channel,username,ts) {
+	var onSub = function (channelName, username, ts) {
 		var content = {
 			name: username,
-			channel: channel,
+			channel: channelName,
 			ts: ts
 		};
 		log('Sub ' + username);
 		nodecg.sendMessage('subscription', content);
-
 	};
 
-	var onUpdate = function(channel,data) {
-		this.emit('update',channel,data);
-		this.nodecg.sendMessage('update', channel,data);
-	}
+	var onUpdate = function (channel, data) {
+		this.emit('update', channel, data);
+		this.nodecg.sendMessage('update', channel, data);
+	};
 
 	function createLive() {
 		live = new LiveLoading(nodecg);
-		live.on('follow',onFollow.bind(this));
-		live.on('sub',onSub.bind(this));
-		live.on('update',onUpdate.bind(this));
 	}
 
 	function addChannels() {
 		var self = this;
-		nodecg.bundleConfig.channels.forEach(function(channel) {
-			if(channelCache[channel] === undefined) {
-				channelCache[channel] = new Channel(channel,nodecg,live);
-				live.addChannel(channelCache[channel]);
+		nodecg.bundleConfig.channels.forEach(function (channelName) {
+			if (channelCache[channelName] === undefined) {
+				var channel = new Channel(channelName, nodecg, live);
+				channel.on('follow', onFollow.bind(self, channelName));
+				channel.on('sub', onSub.bind(self, channelName));
+				channel.on('update', onUpdate.bind(self, channelName));
+				// setTimeout(function() {
+				// 	setInterval(function() {
+				// 		channel.onUpdate('channel:000:resubscribed', {user:{username:'Bamb'}});
+				// 	},1000);
+				// },3000);
+				channelCache[channelName] = channel;
 			}
 		});
 	}
 
 	createLive();
 	addChannels();
+	//Static'ed to prim atm
+	var channel = 'ProbablePrime';
+	nodecg.listenFor('getFollows', function (value, cb) {
+		if (typeof cb === 'function') {
+			channelCache[channel].findUnDismissedFollows().then(function (follows) {
+				cb(null, follows);
+			}).catch(function (err) {
+				cb(err, null);
+			});
+		}
+	});
+	nodecg.listenFor('getSubscriptions', function (value, cb) {
+		if (typeof cb === 'function') {
+			channelCache[channel].findUnDismissedSubscriptions().then(function (subscriptions) {
+				cb(null, subscriptions);
+			}).catch(function (err) {
+				cb(err, null);
+			});
+		}
+	});
 
-}
+	nodecg.listenFor('dismissFollow', function (value) {
+		channelCache[channel].dismissFollow(value);
+	});
+	nodecg.listenFor('dismissSubscription', function (value) {
+		channelCache[channel].dismissSubscription(value);
+	});
+};
